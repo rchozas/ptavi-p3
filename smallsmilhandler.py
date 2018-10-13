a2d5dd3bdb4c0b2ac56e0c02ae7d452a42dac920
Raquel Chozas <r.chozas@alumnos.urjc.es>
2018-10-13 14:17:54 +0200

    intro etiquetas

--- smallsmilhandler.py
+++ smallsmilhandler.py
@@ -1,4 +1,26 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
+from xml.sax import make_parser
+from xml.sax.handler import ContentHandler
 
+class SmallSmilHandler(ContentHandler):
+
+    def __init__(self):
+        self.lista = []
+        self.dic_etiq = {'root-layout': ['width', 'height', 'background-color'],
+                        'region': ['id','top','bottom', 'left', 'right'],
+                         'img': ['src', 'region', 'begin', 'dur'],
+                        'audio': ['src', 'begin', 'dur'],
+                        'textstream'['src', 'region']}
+    def get_tags(self):
+
+
+if __name__ == "__main__":
+    """
+    Programa principal
+    """
+    parser = make_parser()
+    cHandler = ChistesHandler()
+    parser.setContentHandler(cHandler)
+    parser.parse(open())
