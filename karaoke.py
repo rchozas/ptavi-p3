162207b591beecb6ffbbe4913b1e9f50b5e46824
Raquel Chozas <r.chozas@alumnos.urjc.es>
2018-10-15 21:28:01 +0200

    str

--- karaoke.py
+++ karaoke.py
@@ -5,17 +5,36 @@ from xml.sax import make_parser
 from xml.sax.handler import ContentHandler
 from smallsmilhandler import SmallSMILHandler
 import sys
+import json
+import urllib
+
 
 class KaraokeLocal(SmallSMILHandler):
-"""
-    parser = make_parser()
-    cHandler = SmallSMILHandler()
-    parser.setContentHandler(cHandler)
-    parser.parse(open(  )) """
+
+    def __init__(self, fichero):
+        parser = make_parser()
+        cHandler = SmallSMILHandler()
+        parser.setContentHandler(cHandler)
+        parser.parse(open(fichero))
+        self.info = cHandler.get_tags()
+        
+        
+    def __str__(self):
+        linea = " "
+        for elemento in self.info:
+            linea = linea + elemento[0]
+            atributos = elemento[1].items()
+            for nombre, valor in atributos:
+                linea = linea + "\t" + nombre + "=" + '"' + valor + '"'
+            linea = linea + "\n"
+        return linea 
+        
     
+        
 if __name__ == "__main__":
     try:
         fichero = sys.argv[1]
+        
     except IndexError:
         sys.exit("Usage: python3 karaoke.py file.smil")
 
