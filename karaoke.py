cff99dfdf255b65cd6db552fbfcb59f2075784d1
Raquel Chozas <r.chozas@alumnos.urjc.es>
2018-10-15 18:01:22 +0200

    iniciando ...

--- karaoke.py
+++ karaoke.py
@@ -1,4 +1,21 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
+from xml.sax import make_parser
+from xml.sax.handler import ContentHandler
+from smallsmilhandler import SmallSMILHandler
+import sys
+
+class KaraokeLocal(SmallSMILHandler):
+"""
+    parser = make_parser()
+    cHandler = SmallSMILHandler()
+    parser.setContentHandler(cHandler)
+    parser.parse(open(  )) """
+    
+if __name__ == "__main__":
+    try:
+        fichero = sys.argv[1]
+    except IndexError:
+        sys.exit("Usage: python3 karaoke.py file.smil")
 
