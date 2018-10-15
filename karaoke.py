f17f4e04fcca911895b0b620cbee898bc529593e
Raquel Chozas <r.chozas@alumnos.urjc.es>
2018-10-15 22:34:04 +0200

    pep8

--- karaoke.py
+++ karaoke.py
@@ -17,8 +17,7 @@ class KaraokeLocal(SmallSMILHandler):
         parser.setContentHandler(cHandler)
         parser.parse(open(fichero))
         self.info = cHandler.get_tags()
-        
-        
+
     def __str__(self):
         linea = " "
         for elemento in self.info:
@@ -28,24 +27,24 @@ class KaraokeLocal(SmallSMILHandler):
                 linea = linea + "\t" + nombre + "=" + '"' + valor + '"'
             linea = linea + "\n"
         return linea
-        
+
     def to_json(self, fichero, fjson=""):
         if fjson == "":
             fjson = fichero.split(".")[0] + ".json"
         with open(fjson, 'w') as fichero_json:
-            json.dump(self.info, fichero_json, sort_keys=True, indent=4) 
-            
+            json.dump(self.info, fichero_json, sort_keys=True, indent=4)
+
     def do_local(self):
         for elemento in self.info:
-            dicc= elemento[1]
+            dicc = elemento[1]
             for atributo in dicc:
                 if atributo == 'src':
                     if dicc[atributo].split("/")[0] == "http:":
-                        urllib.request.urlretrieve(dicc[atributo],
-                                dicc[atributo].split("/")[-1])
-                        dicc[atributo] = dicc[atributo].split("/")[-1]        
-    
-        
+                        urllib.request.urlretrieve
+                        (dicc[atributo], dicc[atributo].split("/")[-1])
+                        dicc[atributo] = dicc[atributo].split("/")[-1]
+
+
 if __name__ == "__main__":
     try:
         fichero = sys.argv[1]
@@ -57,4 +56,3 @@ if __name__ == "__main__":
         print(karaoke)
     except IndexError:
         sys.exit("Usage: python3 karaoke.py file.smil")
-
